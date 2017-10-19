483423d44126b506aeba5ae9c5519948ccfa0547
Joell Jones <joelljones86@gmail.com>
2017-10-19 04:57:30 -0400

    Dynamically generate a bomb board

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -9,3 +9,15 @@ const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
   }
   return board;
 };
+
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+  let board = [];
+  for (i = 0; i < numberOfRows; i++) {
+    let row = [];
+    for (j = 0; j < numberOfColumns; j++) {
+      row.push(null);
+    }
+    board.push(row);
+  }
+  return board;
+};
